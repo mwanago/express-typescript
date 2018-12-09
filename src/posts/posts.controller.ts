6762f64a17d41c1afdc1fa177d91af7c4691daa6
Marcin Wanago <wanago.marcin@gmail.com>
2018-12-09 03:54:04 +0100

    feat(Posts): add a way to list, create, modify and delete posts

--- src/posts/posts.controller.ts
+++ src/posts/posts.controller.ts
@@ -1,36 +1,68 @@
 import * as express from 'express';
 import Controller from '../interfaces/controller.interface';
 import Post from './post.interface';
+import postModel from './posts.model';
 
 class PostsController implements Controller {
   public path = '/posts';
   public router = express.Router();
-
-  private posts: Post[] = [
-    {
-      author: 'Marcin',
-      content: 'Dolor sit amet',
-      title: 'Lorem Ipsum',
-    },
-  ];
+  private post = postModel;
 
   constructor() {
     this.initializeRoutes();
   }
 
-  public initializeRoutes() {
+  private initializeRoutes() {
     this.router.get(this.path, this.getAllPosts);
+    this.router.get(`${this.path}/:id`, this.getThePostById);
+    this.router.put(`${this.path}/:id`, this.modifyThePost);
+    this.router.delete(`${this.path}/:id`, this.deleteAPost);
     this.router.post(this.path, this.createAPost);
   }
 
-  public getAllPosts = (request: express.Request, response: express.Response) => {
-    response.send(this.posts);
+  private getAllPosts = (request: express.Request, response: express.Response) => {
+    this.post.find()
+      .then((posts) => {
+        response.send(posts);
+      });
   }
 
-  public createAPost = (request: express.Request, response: express.Response) => {
-    const post: Post = request.body;
-    this.posts.push(post);
-    response.send(post);
+  private getThePostById = (request: express.Request, response: express.Response) => {
+    const id = request.params.id;
+    this.post.findById(id)
+      .then((post) => {
+        response.send(post);
+      });
+  }
+
+  private modifyThePost = (request: express.Request, response: express.Response) => {
+    const id = request.params.id;
+    const postData: Post = request.body;
+    this.post.findByIdAndUpdate(id, postData, { new: true })
+      .then((post) => {
+        response.send(post);
+      });
+  }
+
+  private createAPost = (request: express.Request, response: express.Response) => {
+    const postData: Post = request.body;
+    const createdPost = new this.post(postData);
+    createdPost.save()
+      .then((savedPost) => {
+        response.send(savedPost);
+      });
+  }
+
+  private deleteAPost = (request: express.Request, response: express.Response) => {
+    const id = request.params.id;
+    this.post.findByIdAndDelete(id)
+      .then((successResponse) => {
+        if (successResponse) {
+          response.send(200);
+        } else {
+          response.send(404);
+        }
+      });
   }
 }
 
---
+++ src/posts/posts.model.ts
@@ -0,0 +1,12 @@
+import * as mongoose from 'mongoose';
+import Post from './post.interface';
+
+const postSchema = new mongoose.Schema({
+  author: String,
+  content: String,
+  title: String,
+});
+
+const postModel = mongoose.model<Post & mongoose.Document>('Post', postSchema);
+
+export default postModel;
